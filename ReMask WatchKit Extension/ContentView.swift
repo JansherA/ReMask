9ba6aa25e69fc046c11e11a9b951fb205a01e4d0
JansherA <jazmat22@choate.edu>
2020-10-25 17:20:37 -0400

    Committing files

---
+++ ReMask WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ReMask WatchKit Extension
+//
+//  Created by Jansher Azmat on 10/25/20.
+//  Copyright © 2020 Jansher Azmat. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Is this correct?")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ReMask WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  ReMask WatchKit Extension
+//
+//  Created by Jansher Azmat on 10/25/20.
+//  Copyright © 2020 Jansher Azmat. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
